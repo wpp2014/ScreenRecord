a490cb9ad4e7301419f6c7aaa52ce5c6d17d2017
wangpengpeng <wangpengpeng@qtrade.com.cn>
2021-09-29 14:49:27 +0800

    修改日志文件路径，与exe同级目录

--- screen_record/src/main.cc
+++ screen_record/src/main.cc
@@ -5,14 +5,36 @@
 
 #include "glog/logging.h"
 #include "screen_record/src/main_window.h"
+#include "screen_record/src/util/string.h"
+
+const char kLogDirName[] = "log";
+
+void InitLogging() {
+  QString exe_path_qt = QCoreApplication::applicationFilePath();
+  exe_path_qt = QDir::toNativeSeparators(exe_path_qt);
+
+  QDir log_dir(QCoreApplication::applicationDirPath());
+  if (!log_dir.exists(kLogDirName)) {
+    log_dir.mkdir(kLogDirName);
+  }
+
+  QString log_dir_qt = log_dir.absolutePath() + "/" + kLogDirName;
+  log_dir_qt = QDir::toNativeSeparators(log_dir_qt);
+
+  std::wstring exe_path_c = exe_path_qt.toStdWString();
+  std::string exe_path = SysWideToMultiByte(exe_path_c, CP_ACP);
+
+  google::InitGoogleLogging(exe_path.c_str());
+  FLAGS_alsologtostderr = true;
+  FLAGS_log_dir = SysWideToMultiByte(log_dir_qt.toStdWString(), CP_ACP);
+}
 
 int main(int argc, char** argv) {
   QApplication app(argc, argv);
 
   CoInitialize(NULL);
 
-  google::InitGoogleLogging(argv[0]);
-  FLAGS_alsologtostderr = true;
+  InitLogging();
 
   // 设置编码格式为UTF-8
   QTextCodec* codec = QTextCodec::codecForName("UTF-8");
